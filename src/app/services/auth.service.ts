f970b1f6cc6b50a535d0cf5cfeb08157476bbd69
Juan <matareyes91@hotmail.com>
2021-01-22 18:55:50 -0300

    Simple User

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private URL = 'http://localhost:4000'
+
+  constructor(private http: HttpClient) { }
+
+  
+}
